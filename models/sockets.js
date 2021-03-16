de8d76430fbb019972d80e1657d0a29a685777b3
robertramosastudillo <robertramosastudillo@gmail.com>
2021-03-16 13:52:46 -0500

    Se trabajo la implementcion de lectura de token en el backend

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,9 @@
+const {
+  usuarioConectado,
+  usuarioDesconectado,
+} = require("../controllers/sockets");
+const { comprobarJWT } = require("../helpers/jwt");
+
 class Sockets {
   constructor(io) {
     this.io = io;
@@ -7,7 +13,20 @@ class Sockets {
 
   socketEvents() {
     //   On connection
-    this.io.on("connection", (socket) => {});
+    this.io.on("connection", async (socket) => {
+      const [valido, uid] = comprobarJWT(socket.handshake.query["x-token"]);
+
+      if (!valido) {
+        console.log("Socket no identificado");
+        return socket.disconnect();
+      }
+
+      await usuarioConectado(uid);
+
+      socket.on("disconnect", async () => {
+        await usuarioDesconectado(uid);
+      });
+    });
   }
 }
 
